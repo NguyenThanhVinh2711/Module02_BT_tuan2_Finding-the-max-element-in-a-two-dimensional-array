14648a82ae6ebae906f225df5cc76764c5419e39
boomka <vinh99657@gmail.com>
2022-08-08 11:29:59 +0700

    tuan1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+        int[][] arr = new int[2][2];
+        Scanner scanner = new Scanner(System.in);
+        for (int a = 0; a < 2; a++) {
+            for (int b = 0; b < 2; b++) {
+                System.out.println("Nhap phan tu thu [" + a + "," + b + "] :" );
+                arr[a][b] = scanner.nextInt();
+            }
+        }
         int max = arr[0][0];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
